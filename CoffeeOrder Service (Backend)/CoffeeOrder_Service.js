c087f162cca85eaa84b2c15d352a8e3de0137130
DaLammel <ga78wix@mytum.de>
2018-05-14 13:04:59 +0200

    added MachineState Object to address space, added more documentation, wrapped address space construction in a function

--- CoffeeOrder Service (Backend)/CoffeeOrder_Service.js
+++ CoffeeOrder Service (Backend)/CoffeeOrder_Service.js
@@ -14,6 +14,17 @@ The CoffeeOrder Service offers the following Services:
 /* global console, require */
 var opcua = require("node-opcua");
 
+// ToDo: implement MySQL database
+// var mysql = require("mysql");
+
+// declaration of internal Variables, used to handle the OPC UA Variables before writing them to a DB
+var valueCoffeeLevel = 60;	//ToDo: initialize with 0, 100 is only for testing
+var valueWaterLevel = 40;	//ToDo: initialize with 0, 100 is only for testing
+var valueCleanlinessLevel = 80;	//ToDo: initialize with 0, 100 is only for testing
+var valueCoffeeQuantity = 225;	//ToDo: initialize with 0, 225 is only for testing
+var valueCoffeeStrength = 4;	//ToDo: initialize with 0, 4 is only for testing
+var valueMilkQuantity = 25;	//ToDo: initialize with 0, 25 is only for testing
+
 
 // instantiate Server
 var server = new opcua.OPCUAServer({
@@ -22,6 +33,7 @@ var server = new opcua.OPCUAServer({
 	// defaultSecureTokenLifetime: 60000, // the default secure token life time in ms. /* Breaks the server for some reason. Error "Could not connect to server: BadInvalidArgument" */
 	// securityPolicies= [SecurityPolicy.None, SecurityPolicy.Basic128Rsa15, SecurityPolicy.Basic256] // ToDo: Security Implementierung
 	// securityModes= [MessageSecurityMode.NONE, MessageSecurityMode.SIGN, MessageSecurityMode.SIGNANDENCRYPT] // ToDo: Security Implementierung
+	// allowAnonymous= true,	// Tells if the server default endpoints should allow anonymous connection. Default: true
 	buildInfo : {
         productName: "MeinTestServer2",
         buildNumber: "12345",
@@ -30,128 +42,225 @@ var server = new opcua.OPCUAServer({
 });
 
 function post_initialize() {
-    console.log("initialized");
-    var addressSpace = server.engine.addressSpace;
 	
-	// ToDo: add Enum Datatype for Control Panel Buttons in Codesys
-
-	
-	// Add CoffeeOrder Object to the namespace
-    var CoffeeOrderObject = addressSpace.addObject({
-        organizedBy: addressSpace.rootFolder.objects,
-        browseName: "CoffeeOrder",
-		displayName: "CoffeeOrder Object",
-		description: "Das Objekt für eine Kaffeebestellung. Enthält Variablen für die Kaffee- und Milchmenge sowie für die Kaffeestärke. Außerdem stellt es Methoden bereit, mit der man die Kaffeebestellung an die Steuerung weitergeben kann.",
-		nodeId: "ns=1;s=CoffeeOrder"	// die NodeID des CoffeeOrder Objects
-    });
-	
-	// Add CoffeeQuantity Variable to CoffeeOrder Object
-	CoffeeQuantity = addressSpace.addVariable({
-		componentOf: CoffeeOrderObject,
-		browseName: "CoffeeQuantity",
-		dataType: opcua.DataType.UInt16
-	});
-	
-	// Add MilkQuantity Variable to CoffeeOrder Object
-	CoffeeQuantity = addressSpace.addVariable({
-		componentOf: CoffeeOrderObject,
-		browseName: "MilkQuantity",
-		dataType: opcua.DataType.UInt16
-	});
-	
-	// Add CoffeeStrength Variable to CoffeeOrder Object
-	CoffeeQuantity = addressSpace.addVariable({
-		componentOf: CoffeeOrderObject,
-		browseName: "CoffeeStrength",
-		dataType: opcua.DataType.Byte
-	});
-	
-    var toButtonMethod = addressSpace.addMethod(CoffeeOrderObject, {		// ToDo: Here we add the method shell (without the actual functional logic) to the OPC UA Server Address Space
+	//	declaration
+	function construct_my_address_space(server) {
+		var addressSpace = server.engine.addressSpace;
 		
-		browseName: "toButton",
+		// Add MachineState Object to the namespace
+		var MachineStateObject = addressSpace.addObject({
+			organizedBy: addressSpace.rootFolder.objects,
+			browseName: "MachineState",
+			displayName: "MachineState Object",
+			description: "Das Objekt für die Maschinendaten der Kaffeemaschine. Enthält Variablen wie Kaffeestand, Wasserstand, Reinigungszustand, ... zur Verwendung für Monitoring.",
+			nodeId: "ns=1;s=MachineState"	// The NodeID of the MachineState Object: NamespaceIndex = 1, IdentifierType = string, Identifier = MachineState
+		});
 		
-		// These are the Input Arguments that must be passed to the method upon calling it
-		inputArguments: [
-			{
-				name: "CoffeeQuantity",
-				description: {text: "Die Menge an Kaffee in der Bestellung, in ml"},
-				dataType: opcua.DataType.UInt32
-			},
-			{
-				name: "MilkQuantity",
-				description: {text: "Die Menge an Milch in der Bestellung, in ml"},
-				dataType: opcua.DataType.UInt32
-			}
-		],
-		
-		// These are the output arguments that the method returns when called
-		outputArguments: [
-			{
-				name: "buttonToPress",
-				description: {text: "Virtueller Knopf auf dem Bedienpanel, der in CODESYS die Kaffeemaschine bedient"},
-				dataType: opcua.DataType.UInt32 // ToDo: change to my enum datatype CPbutton
-			}
-		]
-	/*
-        browseName: "Bell",
-
-        inputArguments: [
-            {
-                name: "nBells",
-                description: {text: "spezifiziert wie oft der Server kläffen soll"},
-                dataType: opcua.DataType.UInt32
-            }, {
-                name: "lautstaerke",
-                description: {text: "spezifiziert die Größe des Hundes [0 = klein ,100 = groß]"},
-                dataType: opcua.DataType.UInt32
+		// Add CoffeeLevel Variable to MachineState Object
+		var CoffeeLevel = addressSpace.addVariable({
+			componentOf: MachineStateObject,
+			dataType: opcua.DataType.UInt16,	// ToDo: Vielleicht eignet sich ein anderer Datentyp besser, noch mal evaluieren sobald ich die Daten im Frontend verwende.
+			description: "Die Variable für den Kaffeestand in der Kaffeemaschine. Gibt an, wie voll der Kaffeebohnen- oder -pulverbehälter prozentual ist. Der Wert sollte also zwischen 0 (leer) und 100 (voll) liegen.",
+			browseName: "CoffeeLevel",
+			value: {	// implements get and set functions for the value
+                get: function () {
+                    return new opcua.Variant({dataType: opcua.DataType.UInt16, value: valueCoffeeLevel });
+                },
+                set: function (variant) {
+                    valueCoffeeLevel = parseInt(variant.value);
+                    return opcua.StatusCodes.Good;
+                }
             }
-        ],
-
-        outputArguments: [{
-            name: "Bells",
-            description: {text: "Die generierten Laute"},
-            dataType: opcua.DataType.String,
-            valueRank: 1
-        }]
-		*/
-    });
-
-    // optionally, we can adjust userAccessLevel attribute
-    //toButtonMethod.outputArguments.userAccessLevel = opcua.makeAccessLevel("CurrentRead");
-    //toButtonMethod.inputArguments.userAccessLevel = opcua.makeAccessLevel("CurrentRead");
-    
-
-    /* toButtonMethod.bindMethod(function (inputArguments, context, callback) {	// ToDo: Here we add functional logic to the created method shell.
+		});
 		
-		// actual method logic code
+		// Add WaterLevel Variable to MachineState Object	
+		var WaterLevel = addressSpace.addVariable ({
+			componentOf: MachineStateObject,
+			dataType: opcua.DataType.UInt16,	// ToDo: Vielleicht eignet sich ein anderer Datentyp besser, noch mal evaluieren sobald ich die Daten im Frontend verwende.
+			description: "Die Variable für den Wasserstand in der Kaffeemaschine. Gibt an, wie voll der Wasserbehälter prozentual ist. Der Wert sollte also zwischen 0 (leer) und 100 (voll) liegen.",
+			browseName: "WaterLevel",
+			value: {	// implements get and set functions for the value
+                get: function () {
+                    return new opcua.Variant({dataType: opcua.DataType.UInt16, value: valueWaterLevel });
+                },
+                set: function (variant) {
+                    valueWaterLevel = parseInt(variant.value);
+                    return opcua.StatusCodes.Good;
+                }
+            }
+		});
 		
-        // local variables for internal use within the function
-		var nbBarks = inputArguments[0].value;
-        var volume = inputArguments[1].value;
-
-        console.log("Hallo Nachbar, ich werde ", nbBarks, " mal Bellen!");
-        console.log("Meine Größe ist ca. ", volume, " von 100");
-        var sound_volume = Array(volume).join("!");
-
-        var barks = [];
-        for (var i = 0; i < nbBarks; i++) {
-            barks.push("Whaff" + sound_volume);
-        }
-
-        var callMethodResult = {
-            statusCode: opcua.StatusCodes.Good,
-            outputArguments: [{
-                dataType: opcua.DataType.String,
-                arrayType: opcua.VariantArrayType.Array,
-                value: barks
-            }]
-        };
-        callback(null, callMethodResult);
+		// Add Cleanliness Variable to MachineState Object
+		var Cleanliness = addressSpace.addVariable ({
+			organizedBy: MachineStateObject,
+			dataType: opcua.DataType.UInt16,	// ToDo: Vielleicht eignet sich ein anderer Datentyp besser, noch mal evaluieren sobald ich die Daten im Frontend verwende.
+			description: "Die Variable für die Sauberkeit der Kaffeemaschine. Gibt einen ungefähren Richtwert an, wie Sauber die Maschine ist. Der Wert sollte zwischen 100 (frisch gereinigt) und 0 (Reinigung steht an) liegen.",
+			browseName: "Cleanliness",
+			value: {	// implements get and set functions for the value
+                get: function () {
+                    return new opcua.Variant({dataType: opcua.DataType.UInt16, value: valueCleanlinessLevel });
+                },
+                set: function (variant) {
+                    valueCleanlinessLevel = parseInt(variant.value);
+                    return opcua.StatusCodes.Good;
+                }
+            }
+		});
 		
-    });*/
+		// ToDo: add Enum Datatype for Control Panel Buttons in Codesys
+
+		
+		// Add CoffeeOrder Object to the namespace
+		var CoffeeOrderObject = addressSpace.addObject({
+			organizedBy: addressSpace.rootFolder.objects,
+			browseName: "CoffeeOrder",
+			displayName: "CoffeeOrder Object",
+			description: "Das Objekt für eine Kaffeebestellung. Enthält Variablen für die Kaffee- und Milchmenge sowie für die Kaffeestärke. Außerdem stellt es Methoden bereit, mit der man die Kaffeebestellung an die Steuerung weitergeben kann.",
+			nodeId: "ns=1;s=CoffeeOrder"	// The NodeID of the CoffeeOrder Object: NamespaceIndex = 1, IdentifierType = string, Identifier = CoffeeOrder
+		});
+		
+		// Add CoffeeQuantity Variable to CoffeeOrder Object
+		CoffeeQuantity = addressSpace.addVariable({
+			componentOf: CoffeeOrderObject,
+			browseName: "CoffeeQuantity",
+			dataType: opcua.DataType.UInt16,
+			description: "Die Variable für die Kaffeemenge, in ml.",
+			value: {	// implements get and set functions for the value
+                get: function () {
+                    return new opcua.Variant({dataType: opcua.DataType.UInt16, value: valueCoffeeQuantity });
+                },
+                set: function (variant) {
+                    valueCoffeeQuantity = parseInt(variant.value);
+                    return opcua.StatusCodes.Good;
+                }
+            }
+		});
+		
+		// Add MilkQuantity Variable to CoffeeOrder Object
+		CoffeeQuantity = addressSpace.addVariable({
+			componentOf: CoffeeOrderObject,
+			browseName: "MilkQuantity",
+			dataType: opcua.DataType.UInt16,
+			description: "Die Variable für die Milchmenge, in ml.",
+			value: {	// implements get and set functions for the value
+                get: function () {
+                    return new opcua.Variant({dataType: opcua.DataType.UInt16, value: valueMilkQuantity });
+                },
+                set: function (variant) {
+                    valueMilkQuantity = parseInt(variant.value);
+                    return opcua.StatusCodes.Good;
+                }
+            }
+		});
+		
+		// Add CoffeeStrength Variable to CoffeeOrder Object
+		CoffeeQuantity = addressSpace.addVariable({
+			componentOf: CoffeeOrderObject,
+			browseName: "CoffeeStrength",
+			dataType: opcua.DataType.Byte,
+			description: "Die Variable für die Kaffeestärke. Möglich sind die Werte {1, 2, 3, 4, 5}.",
+			value: {	// implements get and set functions for the value
+                get: function () {
+                    return new opcua.Variant({dataType: opcua.DataType.UInt16, value: valueCoffeeStrength });
+                },
+                set: function (variant) {
+                    valueCoffeeStrength = parseInt(variant.value);
+                    return opcua.StatusCodes.Good;
+                }
+            }
+		});
+		
+		var toButtonMethod = addressSpace.addMethod(CoffeeOrderObject, {		// ToDo: Here we add the method shell (without the actual functional logic) to the OPC UA Server Address Space
+			
+			browseName: "toButton",
+			
+			// These are the Input Arguments that must be passed to the method upon calling it
+			inputArguments: [
+				{
+					name: "CoffeeQuantity",
+					description: {text: "Die Menge an Kaffee in der Bestellung, in ml"},
+					dataType: opcua.DataType.UInt32
+				},
+				{
+					name: "MilkQuantity",
+					description: {text: "Die Menge an Milch in der Bestellung, in ml"},
+					dataType: opcua.DataType.UInt32
+				}
+			],
+			
+			// These are the output arguments that the method returns when called
+			outputArguments: [
+				{
+					name: "buttonToPress",
+					description: {text: "Virtueller Knopf auf dem Bedienpanel, der in CODESYS die Kaffeemaschine bedient"},
+					dataType: opcua.DataType.UInt32 // ToDo: change to my enum datatype CPbutton
+				}
+			]
+		/*
+			browseName: "Bell",
+
+			inputArguments: [
+				{
+					name: "nBells",
+					description: {text: "spezifiziert wie oft der Server kläffen soll"},
+					dataType: opcua.DataType.UInt32
+				}, {
+					name: "lautstaerke",
+					description: {text: "spezifiziert die Größe des Hundes [0 = klein ,100 = groß]"},
+					dataType: opcua.DataType.UInt32
+				}
+			],
+
+			outputArguments: [{
+				name: "Bells",
+				description: {text: "Die generierten Laute"},
+				dataType: opcua.DataType.String,
+				valueRank: 1
+			}]
+			*/
+		});
+
+		// optionally, we can adjust userAccessLevel attribute
+		//toButtonMethod.outputArguments.userAccessLevel = opcua.makeAccessLevel("CurrentRead");
+		//toButtonMethod.inputArguments.userAccessLevel = opcua.makeAccessLevel("CurrentRead");
+		
+
+		/* toButtonMethod.bindMethod(function (inputArguments, context, callback) {	// ToDo: Here we add functional logic to the created method shell.
+			
+			// actual method logic code
+			
+			// local variables for internal use within the function
+			var nbBarks = inputArguments[0].value;
+			var volume = inputArguments[1].value;
+
+			console.log("Hallo Nachbar, ich werde ", nbBarks, " mal Bellen!");
+			console.log("Meine Größe ist ca. ", volume, " von 100");
+			var sound_volume = Array(volume).join("!");
+
+			var barks = [];
+			for (var i = 0; i < nbBarks; i++) {
+				barks.push("Whaff" + sound_volume);
+			}
+
+			var callMethodResult = {
+				statusCode: opcua.StatusCodes.Good,
+				outputArguments: [{
+					dataType: opcua.DataType.String,
+					arrayType: opcua.VariantArrayType.Array,
+					value: barks
+				}]
+			};
+			callback(null, callMethodResult);
+			
+		});*/
+
+	}
+	//	end of declaration
+	
+    console.log("initialized");
+	construct_my_address_space(server);	// Call function to construct the server address space.
 
 }
-
 server.initialize(post_initialize);
 
 server.start(function () {
